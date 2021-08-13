e8ce4f7da3ba4dd8269585aeb5b2b207ce35b6ac
x50020344 <xuning37@huawei.com>
2021-08-13 16:15:07 +0800

    【修改背景】添加关联的对象 【命令】 【修改内容】

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,14 +1,19 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Question, Choice
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
 
 
 class QuestionAdmin(admin.ModelAdmin):
-    # 在管理界面，顺序为 pub_date, question_text
     fieldsets = [
         (None, {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
+    inlines = [ChoiceInline]
 
 
 admin.site.register(Question, QuestionAdmin)
